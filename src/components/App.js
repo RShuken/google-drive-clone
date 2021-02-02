2c87ebf5864bafab46da213174dc9125f03a35a4
rshuken <ryanshuken@gmail.com>
2021-02-01 18:37:12 -0700

    added folder hooks and the ability to add folders

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,34 @@
-import React from "react"
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
-import Login from "./authentication/Login"
-import PrivateRoute from "./authentication/PrivateRoute"
-import ForgotPassword from "./authentication/ForgotPassword"
-import UpdateProfile from "./authentication/UpdateProfile"
-import Signup from "./authentication/Signup"
+import React from 'react';
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Profile from './authentication/Profile';
+import Login from './authentication/Login';
+import PrivateRoute from './authentication/PrivateRoute';
+import ForgotPassword from './authentication/ForgotPassword';
+import UpdateProfile from './authentication/UpdateProfile';
+import Signup from './authentication/Signup';
+import Dashboard from './google-drive/Dashboard'
+
+
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
           <AuthProvider>
             <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
+              {/* Google Drive Routes */}
+              <PrivateRoute exact path='/' component={Dashboard} />
+              
+              {/* Profile Routes */}
+              <PrivateRoute path='/user' component={Profile} />
+              <PrivateRoute path='/update-profile' component={UpdateProfile} />
+              {/* Auth Routes */}
+              <Route path='/signup' component={Signup} />
+              <Route path='/login' component={Login} />
+              <Route path='/forgot-password' component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
-      </div>
-    </Container>
-  )
+  );
 }
 
-export default App
+export default App;
